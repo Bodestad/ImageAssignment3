52713d15a7e37750ca425b40ca93d7e4c796a93f
Bodestad <gustav.bodestad@gmail.com>
2017-02-03 10:19:53 +0100

    Detta är en commit

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@
  * Created by gustavbodestad on 2017-02-03.
  */
 public class Main {
-    
+
     public static void main(String[] args) {
         System.out.print("Hej");
+        System.out.print("HEJHEJ");
     }
 }
