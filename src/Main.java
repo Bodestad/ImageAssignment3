b4191feeaab7776c6bf5461698485825d9b66421
Bodestad <gustav.bodestad@gmail.com>
2017-02-03 14:53:36 +0100

    sdgdg

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-import java.nio.Buffer;
 
 /**
  * Created by gustavbodestad on 2017-02-03.
@@ -12,17 +11,28 @@ public class Main {
     public static void main(String[] args) {
         BufferedImage image;
         BufferedImage image2;
-        Got got = new Got();
+        BufferedImage image3;
+        BufferedImage image4;
         MegatronGraphics mega = new MegatronGraphics();
-        Test test = new Test();
+        Compress compress = new Compress();
+        GotGraphics gotGraphics = new GotGraphics();
         try {
+
+            // Read Mtg-file
             image = mega.read("Images/cartoon.mtg");
-            File outputFileNormal = new File("Images/savedNormal.png");
+            File outputFileNormal = new File("Images/original.png");
             ImageIO.write(image, "png", outputFileNormal);
-            image2 = test.dithering(image);
-            File outputfile = new File("Images/saved.png");
+
+            // Compress image
+            image2 = compress.dithering(image);
+            File outputfile = new File("Images/compressedImg.png");
             ImageIO.write(image2, "png", outputfile);
-            mega.write(image2, "Test.got");
+
+            // Convert GotGraphics to Mtg
+            gotGraphics.write(image2, "Images/Compressed.gotGraphics");
+            image3 = gotGraphics.read("Images/Compressed.gotGraphics");
+            mega.write(image3, "Images/fromGotToMtg.mtg");
+
         } catch (IOException e) {
             e.printStackTrace();
         }
